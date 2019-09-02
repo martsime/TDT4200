ccc350ea4838ec46a144cf5622e8e78362157141
Martin Simensen <simensen94@gmail.com>
2019-09-02 11:47:48 +0200

    Add method to scale image

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -5,9 +5,41 @@
 #define XSIZE 2560 // Size of before image
 #define YSIZE 2048
 
+
+uchar* scale(uchar* old_image, unsigned int scale_factor) {
+    uchar *new_image = calloc(scale_factor * scale_factor * XSIZE * YSIZE * 3, 1);
+
+    const int NEW_XSIZE = XSIZE * scale_factor;
+
+    // Loop over old image
+    for (int y = 0; y < YSIZE; y++) { // Row number
+        for (int x = 0; x < XSIZE; x++) { // Column number
+            for (int c = 0; c < 3; c++) { // Channel number
+
+                // 3D index = [row][column][channe]
+                // 3D index -> 1D index
+                int index = y * XSIZE * 3 + x * 3 + c;
+                uchar color = old_image[index];
+
+                // Set color of all new pixels
+                for (int i = 0; i < scale_factor; i++) {
+                    for (int j = 0; j < scale_factor; j++) {
+                        int new_x = x * scale_factor + i; // New row number
+                        int new_y = y * scale_factor + j; // New column number
+                        int new_index = new_y * NEW_XSIZE * 3 + new_x * 3 + c;
+                        new_image[new_index] = color;
+                    }
+                }
+            }
+        }
+    }
+
+    return new_image;
+}
+
 int main() {
-	uchar *image = calloc(XSIZE * YSIZE * 3, 1); // Three uchars per pixel (RGB)
-	readbmp("before.bmp", image);
+    uchar *image = calloc(XSIZE * YSIZE * 3, 1); // Three uchars per pixel (RGB)
+    readbmp("before.bmp", image);
 
     // Altering image
     for (int i = 0; i < XSIZE * YSIZE * 3; i++) {
@@ -15,7 +47,12 @@ int main() {
         image[i] = 255 - image[i];
     }
 
-	savebmp("after.bmp", image, XSIZE, YSIZE);
-	free(image);
-	return 0;
+    // Scale image
+    unsigned int scale_factor = 2;
+    uchar *new_image = scale(image, scale_factor);
+
+    savebmp("after.bmp", new_image, XSIZE * scale_factor, YSIZE * scale_factor);
+    free(image);
+    free(new_image);
+    return 0;
 }
