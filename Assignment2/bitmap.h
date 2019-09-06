cfedcc41b7c6befd755f467cfa8a6e27cfdec79c
martsime <simensen94@gmail.com>
2019-09-06 15:48:49 +0200

    Add image manipulation methods

--- Assignment2/bitmap.h
+++ Assignment2/bitmap.h
@@ -5,5 +5,7 @@
 typedef unsigned char uchar;
 void savebmp(char *name, uchar *buffer, int x, int y);
 void readbmp(char *filename, uchar *array);
+void invertbmp(uchar* image, int width, int height, int channels);
+void scalebmp(uchar* image, uchar* new_image, int width, int height, int channels, int scale_factor);
 
 #endif
