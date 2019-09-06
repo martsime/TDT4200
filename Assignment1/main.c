f18f2c891d204b672f9e0a5234874d23e806ad01
Martin Simensen <simensen94@gmail.com>
2019-09-06 18:13:41 +0200

    Add comments to assignment 1

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -7,6 +7,7 @@
 
 
 int main() {
+    // Load the image
     uchar *image = calloc(XSIZE * YSIZE * 3, 1); // Three uchars per pixel (RGB)
     readbmp("before.bmp", image);
 
@@ -16,9 +17,13 @@ int main() {
     // Scale image
     unsigned int scale_factor = 2;
     uchar *new_image = scalebmp(image, XSIZE, YSIZE, 3, scale_factor);
-
+    
+    // Save the image
     savebmp("after.bmp", new_image, XSIZE * scale_factor, YSIZE * scale_factor);
+
+    // Free memory
     free(image);
     free(new_image);
+
     return 0;
 }
